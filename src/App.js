cc864cb70d155baddd0e90467cb714d17a47088a
Otacilia <otaciliatimoteo@hotmail.com>
2022-02-18 15:12:20 -0300

    ecommerce

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import React from 'react';
 import Header from './components/Header'
 import MostarProdutos from './components/MostarProdutos';
 // import {ComponenteFiltro} from './Components/filtro.js';
+import ComponenteFiltro from './components/Filtro';
 
 function App() {
   return (
    <>
        
       <Header/>
-      {/* <ComponenteFiltro/> */}
-
-      <MostarProdutos/>
+      <ComponenteFiltro/> 
+  <MostarProdutos/>
     
 
    </>
